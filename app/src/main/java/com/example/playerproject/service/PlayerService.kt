1839259126b0973fbef1bb09a9501d4e91439130
Denispok <shakhover@yandex.ru>
2020-06-17 03:14:22 +0300

    add audio focus

--- app/src/main/java/com/example/playerproject/service/PlayerService.kt
+++ app/src/main/java/com/example/playerproject/service/PlayerService.kt
@@ -1,15 +1,21 @@
 package com.example.playerproject.service
 
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.media.AudioAttributes
+import android.media.AudioFocusRequest
+import android.media.AudioManager
 import android.net.Uri
 import android.os.Binder
+import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
+import androidx.annotation.RequiresApi
 import androidx.media.MediaBrowserServiceCompat
 import com.example.playerproject.MainActivity
 import com.google.android.exoplayer2.ExoPlayer
@@ -27,7 +33,14 @@ class PlayerService : MediaBrowserServiceCompat() {
         private const val EMPTY_MEDIA_ROOT_ID = "empty_root_id"
         private const val POSITION_UPDATE_TIME_MILLIS = 500L
         private const val PLAYBACK_SPEED = 1f
+        private const val DUCK_VOLUME = 0.2f
     }
+
+    private lateinit var audioManager: AudioManager
+    private lateinit var audioFocusChangeListener: AudioManager.OnAudioFocusChangeListener
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private lateinit var audioFocusRequest: AudioFocusRequest
 
     private lateinit var mediaSession: MediaSessionCompat
     private lateinit var stateBuilder: PlaybackStateCompat.Builder
@@ -40,6 +53,11 @@ class PlayerService : MediaBrowserServiceCompat() {
 
     override fun onCreate() {
         super.onCreate()
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        audioFocusChangeListener = getAudioFocusChangeListener()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            audioFocusRequest = getAudioFocusRequest()
+        }
 
         mediaSession = MediaSessionCompat(applicationContext, "PlayerServiceMediaSession").apply {
             stateBuilder = PlaybackStateCompat.Builder()
@@ -62,6 +80,10 @@ class PlayerService : MediaBrowserServiceCompat() {
 
         metadataBuilder = MediaMetadataCompat.Builder()
         player = ExoPlayerFactory.newSimpleInstance(applicationContext)
+
+        // todo
+        /*val mediaButtonIntent = Intent(Intent.ACTION_MEDIA_BUTTON, null, applicationContext, MediaButtonReceiver::class.java)
+        mediaSession.setMediaButtonReceiver(PendingIntent.getBroadcast(applicationContext, 0, mediaButtonIntent, 0));*/
     }
 
     override fun onDestroy() {
@@ -70,9 +92,58 @@ class PlayerService : MediaBrowserServiceCompat() {
         player.release()
     }
 
+    override fun onLoadChildren(parentId: String, result: Result<MutableList<MediaBrowserCompat.MediaItem>>) {
+        result.sendResult(null)
+    }
+
+    override fun onGetRoot(clientPackageName: String, clientUid: Int, rootHints: Bundle?): BrowserRoot? {
+        return BrowserRoot(EMPTY_MEDIA_ROOT_ID, null)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return PlayerServiceBinder()
+    }
+
+    private fun getAudioFocusChangeListener() = AudioManager.OnAudioFocusChangeListener { focusChange ->
+        when (focusChange) {
+            AudioManager.AUDIOFOCUS_GAIN -> {
+                mediaSession.controller.transportControls.play()
+            }
+
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK -> {
+                player.audioComponent?.volume = DUCK_VOLUME
+            }
+
+            else -> {
+                mediaSession.controller.transportControls.pause()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getAudioFocusRequest(): AudioFocusRequest {
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .build()
+        return AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+            .setOnAudioFocusChangeListener(audioFocusChangeListener)
+            .setAcceptsDelayedFocusGain(false)
+            .setWillPauseWhenDucked(false)
+            .setAudioAttributes(audioAttributes)
+            .build()
+    }
+
     private fun getMediaSessionCallback() = object : MediaSessionCompat.Callback() {
 
         override fun onPlay() {
+            val audioFocus = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                audioManager.requestAudioFocus(audioFocusRequest)
+            } else {
+                audioManager.requestAudioFocus(audioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN)
+            }
+            if (audioFocus != AudioManager.AUDIOFOCUS_REQUEST_GRANTED) return
+
             val currentTrack = playerPlaylist.getCurrent() ?: return
 
             if (currentPlayerUri != currentTrack.uri) {
@@ -86,6 +157,7 @@ class PlayerService : MediaBrowserServiceCompat() {
             }
 
             player.playWhenReady = true
+            player.audioComponent?.volume = 1f
             startPositionLooper()
 
             mediaSession.setPlaybackState(
@@ -112,6 +184,12 @@ class PlayerService : MediaBrowserServiceCompat() {
         }
 
         override fun onStop() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                audioManager.abandonAudioFocusRequest(audioFocusRequest)
+            } else {
+                audioManager.abandonAudioFocus(audioFocusChangeListener)
+            }
+
             currentPlayerUri = null
 
             mediaSession.isActive = false
@@ -146,18 +224,6 @@ class PlayerService : MediaBrowserServiceCompat() {
                 mediaSession.controller.transportControls.play()
             }
         }
-    }
-
-    override fun onLoadChildren(parentId: String, result: Result<MutableList<MediaBrowserCompat.MediaItem>>) {
-        result.sendResult(null)
-    }
-
-    override fun onGetRoot(clientPackageName: String, clientUid: Int, rootHints: Bundle?): BrowserRoot? {
-        return BrowserRoot(EMPTY_MEDIA_ROOT_ID, null)
-    }
-
-    override fun onBind(intent: Intent?): IBinder? {
-        return PlayerServiceBinder()
     }
 
     private fun startPositionLooper() {
